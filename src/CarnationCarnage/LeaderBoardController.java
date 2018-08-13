feb3c8b5828774ac854d9afbfcf94f6c5aa52c4c
Sonya Bell <s3731780@student.rmit.edu.au>
2018-08-14 08:08:37 +1000

    high score txt

--- src/CarnationCarnage/LeaderBoardController.java
+++ src/CarnationCarnage/LeaderBoardController.java
@@ -1,5 +1,6 @@
 package CarnationCarnage;
 
+import java.io.*;
 import java.io.File;
 import java.io.IOException;
 import javafx.application.Application;
@@ -12,40 +13,45 @@ import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.stage.FileChooser.ExtensionFilter;
 
-public class LeaderBoardController extends Application {
+public class LeaderBoardController extends Application implements java.io.Serializable {
+	@FXML
+	private AnchorPane Leader;
 
-    @FXML
-    private AnchorPane Leader;
+	public void initialize() throws IOException {
+		loadHighScore();
+	}
 
-    public void initialize() {
+	public void loadHighScore() throws IOException {
+		FileReader inputReader = new FileReader("HighScore.txt");
+		BufferedReader reader = new BufferedReader(inputReader);
+		String line = null;
+		while ((line = reader.readLine()) != null) {
+			System.out.println(line);
+		}
+		reader.close();
+	}
 
-    }
+	@FXML
+	public void loadClicked() throws IOException {
 
-    @FXML
-    public void loadClicked() throws IOException {
-        FileChooser fileChooser = new FileChooser();
-        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("TXT files (*.txt)", "*.txt");
-        fileChooser.getExtensionFilters().add(extFilter);
-        File file = fileChooser.showOpenDialog(null);
-        System.out.println(file);
-    }
+	}
 
-    @FXML
-    public void backClicked() throws IOException {
-        // Send the user back to the main menu
-        // Load Scene
-        FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(CarnationCarnageMain.class.getResource("/CarnationCarnage/FXMLFiles/mainMenu.fxml"));
-        BorderPane menuScreen = loader.load();
+	@FXML
+	public void backClicked() throws IOException {
+		// Send the user back to the main menu
+		// Load Scene
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(CarnationCarnageMain.class.getResource("/CarnationCarnage/FXMLFiles/mainMenu.fxml"));
+		BorderPane menuScreen = loader.load();
 
-        // Show Scene
-        Stage primaryStage = (Stage) Leader.getScene().getWindow();
-        primaryStage.setScene(new Scene(menuScreen));
-    }
+		// Show Scene
+		Stage primaryStage = (Stage) Leader.getScene().getWindow();
+		primaryStage.setScene(new Scene(menuScreen));
+	}
 
-    @Override
-    public void start(Stage arg0) throws Exception {
-        // TODO Auto-generated method stub
+	@Override
+	public void start(Stage arg0) throws Exception {
+		// TODO Auto-generated method stub
 
-    }
+	}
 }
